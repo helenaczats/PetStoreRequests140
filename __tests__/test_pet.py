0d42b4280e0f4f358d6918c4a829a946cf0374b4
helenaczats <helenaczats@gmail.com>
2024-04-22 14:47:10 +0200

    PetStoreRequests140

--- __tests__/test_pet.py
+++ __tests__/test_pet.py
@@ -1,7 +1,9 @@
 #1- bibliotecas
 import json          # leitor e escritor de arquivos json
 import pytest        # motor/ framework de teste de unidade
-import requests      # framework de teste de API
+import requests
+
+from utils.utils import ler_csv      # framework de teste de API
 
 
 
@@ -87,12 +89,13 @@ def test_put_pet():
     response_body = response.json()
     
     assert response.status_code == 200
-    assert response_body["id"] == pet_id
-    assert response_body["name"] == pet_name
-    assert response_body["category"]["name"] == pet_category_name
-    assert response_body["tags"][0]["name"] == pet_tag_name
-    assert response_body["tags"][0]["id"] == pet_tag_id
-    assert response_body["status"] == "sold"
+    assert response_body['id'] == pet_id
+    assert response_body['name'] == pet_name
+    assert response_body['category']['id'] == pet_category_id
+    assert response_body['category']['name'] == pet_category_name
+    assert response_body['tags'][0]['id'] == pet_tag_id
+    assert response_body['tags'][0]['name'] == pet_tag_name
+    assert response_body['status'] == 'sold'
     
 def test_delete_pet():
     # Configura
@@ -112,3 +115,47 @@ def test_delete_pet():
     assert response_body["message"] == str(pet_id)
     
     
+@pytest.mark.parametrize("pet_id,category_id,category_name,pet_name,tags,status",ler_csv("./fixtures/csv/pets.csv"))
+def test_post_pet_dinamico(pet_id,category_id,category_name,pet_name,tags,status):
+    # Configura
+    pet = {}     # cria uma lista vazia chamada pet
+    pet["id"] = int(pet_id)
+    pet["category"] = {}
+    pet["category"]["id"] = int(category_id)
+    pet["category"]["name"] = category_name
+    pet["name"] = pet_name
+    pet["photoUrls"] = []
+    pet["photoUrls"].append("")
+    pet["tags"] = []
+        
+    tags = tags.split(";")
+    for tag in tags:
+        tag = tag.split("-")
+        tag_formatada = {}
+        tag_formatada["id"] = int(tag[0])
+        tag_formatada["name"] = tag[1]
+        pet["tags"].append(tag_formatada)
+            
+    pet["status"] = status
+        
+    pet = json.dumps(obj=pet, indent=4)
+    print("\n" + pet) # visualiza como ficou o json criado dinamicamente
+        
+    # Executa
+    response = requests.post(
+        url = url,
+        headers = headers,
+        data = pet,
+        timeout = 5
+    )
+            
+    # Compara
+    response_body = response.json()
+        
+    assert response.status_code == 200
+    assert response_body["id"] == int(pet_id)
+    assert response_body["name"] == pet_name
+    assert response_body["status"] == status
+        
+        
+        
